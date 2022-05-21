52695ef3b95553297591d35e98212bfa7bda08c4
Prateek Bhuwania <prateekchinu3011@gmail.com>
2022-05-21 07:53:56 -0400

    cuda version fix

--- slurm.sh
+++ slurm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#DO NOT TOUCH THIS SECTION BEGIN
+
 ####### CUSTOMIZE THIS SECTION FOR YOUR JOB
 #SBATCH --job-name="pikapika"
 #SBATCH --nodes=1
@@ -9,13 +11,19 @@
 #SBATCH --time=08:00:00
 
 echo $CUDA_VISIBLE_DEVICES
-
-module load cuda/11.0
+module load cuda/11.3.1
 module load python/py38-anaconda-2021.05
 source activate /panasas/scratch/grp-lsmatott/ve/condaclone
+#DO NOT TOUCH THIS SECTION END
+
+#Edit here BEGIN
 
 python ./vgg19.py
 
+#Edit here END
+
+#DO NOT TOUCH THIS SECTION BEGIN
 source deactivate /panasas/scratch/grp-lsmatott/ve/condaclone
 module unload python/py38-anaconda-2021.05
-module unload cuda/11.0
+module unload cuda/11.3.1
+#DO NOT TOUCH THIS SECTION END
